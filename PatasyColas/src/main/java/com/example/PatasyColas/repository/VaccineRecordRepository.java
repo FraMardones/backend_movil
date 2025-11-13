b2167ac0e6d23f4feafd0edf1d768393675f9286
FraMardones <168592238+FraMardones@users.noreply.github.com>
2025-11-13 12:59:27 -0300

    Delete PatasyColas directory

--- PatasyColas/src/main/java/com/example/PatasyColas/repository/VaccineRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.PatasyColas.repository;
-
-import com.example.PatasyColas.model.VaccineRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VaccineRecordRepository extends JpaRepository<VaccineRecord, Integer> {
-}
